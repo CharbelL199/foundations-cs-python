8fd2db31c38202b6beb0a2431db1d6d1c19de447
Charbellawlach <94932494+Charbellawlach@users.noreply.github.com>
2023-10-30 19:41:27 +0200

    adding assignment 1

---
+++ temp.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+print(hi)
+This is a temporary script file.
+"""
+#Question 1
+#top = float(input("Please enter top value:"))
+#bottom = float(input("Please enter bottom value:"))
+#val = float(input("Please enter the value:"))    
+
+#if val < top and val > bottom:
+#    print ("True")
+#else:
+ #   print ("False")
+    
+#print(top,bottom,val)
+
+#exercise 2
+# a = int(input("Please enter the first number:"))
+# b = int(input("Please enter the second number:"))
+# c = int(input("Please enter the third number:"))
+
+#max_num = max(a,b,c)
+#min_num = min(a,b,c)
+# print("The maximum value is:",max(a,b,c))
+# print("The minimum value is:",min(a,b,c))
+
+
+#exercise 3
+#grade = int(input("Please enter your grade:"))
+
+#if grade > 89 and grade <=100:
+#    print("A")
+#elif grade > 79 and grade <= 89:
+#    print("B")
+#elif grade > 69 and grade <= 79:
+#    print("C")
+#elif grade > 59 and grade <= 69:
+#    print("D")
+#else:
+#    print("F")
+
+
+
