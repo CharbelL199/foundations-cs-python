559c023a80d5a4530c2154d044f282695ee38028
Charbellawlach <94932494+Charbellawlach@users.noreply.github.com>
2023-11-25 19:18:05 +0200

    update

--- midterm.py
+++ midterm.py
@@ -1,12 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+
 
 #choice 1
 class Tab:
-    def __init__(self,title,url,content=None):
+    def __init__(self,title,url):
         self.title = title
         self.url = url
-        self.content = content
         self.children = []
     
 #for tab list
@@ -14,26 +12,21 @@ class TabManager:
     def __init__(self):
         self.tabs=[]
     
-    def add_tab(self,title,url,content=None,parent_index=None):
+    def add_tab(self,title,url):
         new_tab = Tab(title, url)
         self.tabs.append(new_tab)
         print("Tab",title,"with url",url,"added successfully.")
-        #geeks for geeks 
-        if parent_index is not None:
-            parent_tab = self.tabs[parent_index]
-            parent_tab.children.append(new_tab)
-
+    
     #choice 2    
     def close_tab(self,index=None):
        if index is None or not (0 <= index < len(self.tabs)):
            closed_tab = self.tabs.pop()
-           print("Closed tab ",closed_tab.title,"with URL",closed_tab.url,".")
+           print(f"Closed tab '{closed_tab.title}' with URL '{closed_tab.url}'.")
        else:
            closed_tab = self.tabs.pop(index)
-           print("Closed tab ",closed_tab.title,"with URL",closed_tab.url,".")
+           print(f"Closed tab '{closed_tab.title}' with URL '{closed_tab.url}'.")
            
     #choice 3       
-    # web scraping https://www.geeksforgeeks.org/python-web-scraping-tutoria
     def display_tab_content(self, index=None):
             if not self.tabs:
                 print("No tabs to display.")
@@ -44,50 +37,17 @@ class TabManager:
                 tab_to_display = self.tabs[-1]
             else:
                 tab_to_display = self.tabs[index]
-            
-            print("Tabs:",tab_to_display.title,",URL:",tab_to_display.url)
-            try:
-                # Fetch the HTML content from the URL using requests
-                response = requests.get(tab_to_display.url)
-                response.raise_for_status()
-    
-                # Parse the HTML content using BeautifulSoup
-                soup = BeautifulSoup(response.text, 'html.parser')
-    
-                # Print the parsed HTML content
-                print(soup.prettify())
-
-            except requests.exceptions.RequestException as e:
-                print(f"Error fetching content: {e}")
-                print("Content",tab_to_display.content)
                 
-                        
-    def print_all_tabs(self, tabs=None, indent=""):
+                         
+    def print_all_tabs(self,tabs=None,indent=""):
         if tabs is None:
             tabs = self.tabs
-
-        if not tabs:
-            print("No tabs to display.")
-            return
-
+        
         for tab in tabs:
-            print(indent + tab.title)
+            print(indent+tab.title)
             if tab.children:
-                self.print_all_tabs(tab.children, indent + "  ")
-                
-   
-    def sortingTabs(self):
-        if not self.tabs:
-            print("No tabs to sort")
-            return
-        
-        lengthTabs = len(self.tabs)
-        for i in range(lengthTabs-1):
-            for j in range(0,lengthTabs-i-1):
-                if self.tabs[j].title > self.tabs[j+1].title:
-                    self.tabs[j],self.tabs[j+1]= self.tabs[j+1],self.tabs[j]
-        print("Your tabs have been sorted.")
-        
+                self.print_all_tabs(tab.children,indent + " ")
+
 tab_manager = TabManager()    
 
 while True:
@@ -102,6 +62,7 @@ while True:
     print("8. Import Tabs")
     print("9. Exit")
     choice = input("Enter your choice (1-9): ")
+    print("HI")
     
     if choice == '1':
         title = input("Enter the title:")
@@ -128,16 +89,9 @@ while True:
     elif choice == '4':
         tab_manager.print_all_tabs()
     elif choice == '5':
-        parent_input = input("Enter the index:")
-        try:
-            parent_index = int(parent_input)
-            title = input("Enter the title of nested:")
-            content = input("Enter the url of nested:")
-            tab_manager.add_tab(title, content, parent_index)
-        except ValueError:
-            print("Invalid input.")
+        print()
     elif choice == '6':
-        tab_manager.sortingTabs()
+        print("Exiting the program.")
     elif choice == '7':
         print()
     elif choice == '8':
